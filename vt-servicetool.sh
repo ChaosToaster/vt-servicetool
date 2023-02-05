31b6a952bbe9bd750633561f111819564f2fd3e6
ChaosToasterHH <chaostoasterhh@gmail.com>
2023-02-05 18:28:35 +0100

    Änderungen

--- vt-servicetool.sh
+++ vt-servicetool.sh
@@ -9,11 +9,55 @@
 #-------------------------------------------------------------------------------------
 
 PS3='Willkommen im Hauptmenü. Wähle eine Kategorie: '
+echo ""
+echo "Mit ENTER könnt Ihr das Menu erneut aufrufen, um zugucken, in welchem ihr euch befindet."
+echo ""
+echo "--- HAUPTMENÜ ---"
+echo ""
     select HM in Betriebssysteme Wartung Tools Beenden
-        do
+        do        
             case $HM in
                 Betriebssysteme)
-                    echo "Wähle dein zu installierendes System aus."
+                    select UM1 in Desktop Server zurück
+                        do
+                            case $UM1 in
+                                Desktop)
+                                    select dt1 in LTS LTS2 LTS3 zurück
+                                        do
+                                            case $dt1 in
+                                                LTS)
+                                                ;;
+                                                LTS2)
+                                                ;;
+                                                LTS3)
+                                                ;;
+                                                zurück)
+                                                    echo "Zurück zu Betriebssysteme"; break
+                                                ;;
+                                            esac
+                                        done
+                                ;;
+                                Server)
+                                    select sv1 in LTS LTS2 LTS3 zurück
+                                        do
+                                            case $sv1 in
+                                                LTS)
+                                                ;;
+                                                LTS2)
+                                                ;;
+                                                LTS3)
+                                                ;;
+                                                zurück)
+                                                    echo "Zurück zu Betriebssysteme"; echo ""; break
+                                                ;;
+                                            esac
+                                        done
+                                ;;
+                                zurück)
+                                    echo "Zurück ins Hauptmenü"; echo ""; break
+                                ;;
+                            esac
+                        done
                     ;;
                 Wartung)
                     echo "Wähle den Akt der Wartung des Systems den du durchführen möchtest aus."
@@ -22,8 +66,8 @@ PS3='Willkommen im Hauptmenü. Wähle eine Kategorie: '
                     echo "Hier findest du alles über die Benutzer-/Gruppensteuerung in Linux."
                     ;;
                 Beenden)
-                    break
+                    echo "Das Tool wurde erfolgreich beendet."; break
                     ;;
-                *) echo "ungültige Kategorie $REPLY. Bitte versuche es erneut!";;
+                *) echo "$REPLY ist keine gelistete Option. Bitte versuche es erneut.";;
             esac
-        done;;
+        done
